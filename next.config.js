74f0e702d014ee7fe96158f31b41a253f0ba174b
Daniil <daniilk11@mail.ru>
2025-03-02 23:15:20 +0100

    add config for github pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
-}
+    output: "export", // Enables static export
+    basePath: "/core-lending-frontend", // Replace with your repo name
+    images: { unoptimized: true }, // GitHub Pages does not support Next.js image optimization
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
