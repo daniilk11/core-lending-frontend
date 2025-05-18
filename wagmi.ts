425c3111ca7a2626857d24a2021f903ca26d0a2c
Daniil <daniilk11@mail.ru>
2025-05-18 14:31:58 +0200

    Refactor wagmi configuration to disable unused chains and transports for improved clarity and maintainability.

--- wagmi.ts
+++ wagmi.ts
@@ -1,31 +1,26 @@
 import { http } from 'wagmi'
 import { createConfig } from 'wagmi'
 import {
-  arbitrum,
-  base,
   baseSepolia,
-  mainnet,
-  optimism,
-  polygon,
   sepolia,
 } from 'wagmi/chains'
 
 export const wagmiConfig = createConfig({
   chains: [
     baseSepolia,
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    base,
+    // mainnet,
+    // polygon,
+    // optimism,
+    // arbitrum,
+    // base,
     ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
   ],
   transports: {
-    [mainnet.id]: http(),
-    [polygon.id]: http(),
-    [optimism.id]: http(),
-    [arbitrum.id]: http(),
-    [base.id]: http(),
+    // [mainnet.id]: http(),
+    // [polygon.id]: http(),
+    // [optimism.id]: http(),
+    // [arbitrum.id]: http(),
+    // [base.id]: http(),
     [baseSepolia.id]: http(),
     [sepolia.id]: http(),
   },
